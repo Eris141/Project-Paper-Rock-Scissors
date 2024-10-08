7050eb525c971a449d6ee516e642f8ae9eacc71a
Eris141 <165952691+Eris141@users.noreply.github.com>
2024-10-08 22:24:39 +0200

    feat: run game 5 times, and get results

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@
 function getComputerChoice() {
   let value = '';
   let randomNumber = Math.random();
-  console.log(randomNumber);
   //sepeate the number in 3 parts to get the random value
   if (randomNumber >= 0 && randomNumber <= 1/3) {
     value = 'rock';
@@ -12,31 +11,82 @@ function getComputerChoice() {
   } else {
     value = 'scissor';
   }
-
+  // console.log(value);
   return value;
 }
 
-console.log(getComputerChoice());
 
 //create a function that takes choice
 function getHumanChoice() {
   //ask the user to put the choice
-  let choice = prompt('Enter your choice here. "scissor, paper ,rock"').toLowerCase();
-
+  let choice = prompt("Enter your choice here.", "").toLowerCase();
+  // console.log(choice);
   //check if the user has put the correct choice
-  if (choice === '') {
-    console.log('Please enter, HINT: *scissor, paper, rock*')
-  } else if (choice !== "paper" || choice !== "scissor" || choise !== "rock") {
-    console.log("Allowed words: HINT: *scissor, paper, rock*")
+  if (choice === "") {
+    console.log('Please enter, HINT: *scissor, paper, rock*');
+    return;
+  } else if (choice !== "paper" && choice !== "scissor" && choice !== "rock") {
+    console.log("Wrong words: HINT: *scissor, paper, rock*")
   }
 
   return choice;
 }
 
-console.log(getHumanChoice());
-
-
 //to keep the values of the score player and computer
 let humanScore = 0;
 let computerScore = 0;
+
+//create a function for the game play
+function playRound(humanChoice, computerChoice) {
+  //must have 2 parameters
+  let result = "";
+  //write the code to compare the values
+  if (humanChoice === computerChoice) {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: Tie. `);
+  }
+  
+  //incment the score of th winner
+  //human win
+  if (humanChoice === "rock" && computerChoice === "scissor") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You Win. `)
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You Win. `)
+    humanScore++;
+  } else if (humanChoice === "scissor" && computerChoice === "paper") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You Win. `)
+    humanScore++;
+  }
+
+  //computer win
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You lose. `)
+    computerScore++;
+  } else if (humanChoice === "scissor" && computerChoice === "rock") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You lose. `)
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissor") {
+    result = (`You chose:${humanChoice}, Computer choose: ${computerChoice}: You lose. `)
+    computerScore++;
+  } 
+
+  return result;
+}
+
+
+function playGame() {
+  let rounds = 5;
+  while (rounds > 0) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+  
+    console.log(playRound(humanSelection, computerSelection));
+    let gameScore = `Score: player: ${humanScore} , computer: ${computerScore}`;
+    console.log(gameScore);
+    rounds--;
+  }
+}
+
+playGame();
+
 
